d351cea6a7a3a89aa5c96da8377aeaee6d463731
genia <wayra204genia@gmail.com>
2023-07-25 22:36:35 -0400

    feat:añadiendo imagenes y configurando

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        tomate: "url('../img/tomate.jpg)",
+        tomato: "url('../img/tomato.jpg')",
+        uva: "url('../img/uva.jpg')",
+        verduras: "url('../img/verduras.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      }),
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        Dancing_Script: ["Dancing Script", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
